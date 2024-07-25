89adb408fca9dd1b136414a29c814bc031e3b40e
Pyatakov Oleg <o.pyatakov@adengi.ru>
2024-07-25 13:21:44 +0300

    refresh dist

--- dist/assets/html_tasks-B31Lh0vc.js
+++
@@ -1,1 +0,0 @@
-const i=[{id:"html-1",subject:"HTML",title:"?",description:"?",condition:"?",links:["?"],isDone:!1,isReady:!1}];export{i as H};
---
+++ dist/assets/html_tasks-CJ1vqkDh.js
@@ -0,0 +1,1 @@
+const t=[{id:"html-1",subject:"HTML",title:"?",description:{text:"?"},condition:"?",links:["?"],isDone:!1,isReady:!1}];export{t as H};
